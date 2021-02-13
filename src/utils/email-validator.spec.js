5803c275041ee9ec9634668f1452f2ba25a47e12
Renan Dantas <dantas.renan@icloud.com>
2021-02-13 01:13:21 -0300

    feat: ensure EmailValidator returns false if validator returns false

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -1,6 +1,8 @@
+const validator = require('validator')
+
 class EmailValidator {
   async isValid (email) {
-    return true
+    return validator.isEmail(email)
   }
 }
 
@@ -9,5 +11,12 @@ describe('Email Validator', () => {
     const sut = new EmailValidator()
     const isEmailValid = await sut.isValid('valid_email@mail.com')
     expect(isEmailValid).toBe(true)
+  })
+
+  test('Should return true if validator returns true', async () => {
+    const sut = new EmailValidator()
+    validator.isEmailValid = false
+    const isEmailValid = await sut.isValid('false_email@mail.com')
+    expect(isEmailValid).toBe(false)
   })
 })
