df6c3c302363db4a9aae31dbadc4c9996cffe14d
Renan Dantas <dantas.renan@icloud.com>
2021-02-13 01:20:09 -0300

    feat: move sut creation toa factory helper method

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -6,15 +6,19 @@ class EmailValidator {
   }
 }
 
+const makeSut = () => {
+  return new EmailValidator()
+}
+
 describe('Email Validator', () => {
   test('Should return true if validator returns true', async () => {
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = await sut.isValid('valid_email@mail.com')
     expect(isEmailValid).toBe(true)
   })
 
   test('Should return true if validator returns true', async () => {
-    const sut = new EmailValidator()
+    const sut = makeSut()
     validator.isEmailValid = false
     const isEmailValid = await sut.isValid('false_email@mail.com')
     expect(isEmailValid).toBe(false)
