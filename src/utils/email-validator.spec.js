510cb37211f211421926e576e96690591f1cf941
Renan Dantas <dantas.renan@icloud.com>
2021-02-13 01:29:01 -0300

    feat: ensure EmailValidator calls validate with correct email

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -17,10 +17,16 @@ describe('Email Validator', () => {
     expect(isEmailValid).toBe(true)
   })
 
-  test('Should return true if validator returns true', async () => {
+  test('Should return false if validator returns true', async () => {
     const sut = makeSut()
     validator.isEmailValid = false
     const isEmailValid = await sut.isValid('false_email@mail.com')
     expect(isEmailValid).toBe(false)
+  })
+
+  test('Should call validator with correct email', async () => {
+    const sut = makeSut()
+    await sut.isValid('any_email@mail.com')
+    expect(validator.email).toBe('any_email@mail.com')
   })
 })
