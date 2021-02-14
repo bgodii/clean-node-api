b993afcc77179ddde911644847cb2aafa9b0a46b
Renan Dantas <dantas.renan@icloud.com>
2021-02-14 02:30:44 -0300

    refactor: adjusting coverage

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,5 @@
 module.exports = {
   coverageDirectory: 'coverage',
-  testEnvironment: 'node',
-  collectCoverageFrom: ['**/src/*.js']
+  testEnvironment: 'node'
+  // collectCoverageFrom: ['**/src/*.js']
 }
